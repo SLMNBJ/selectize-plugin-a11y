04ff9d7e3fd0c52065c9eb3d5e62a2366b35c066
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2018-05-28 12:45:30 +0200

    remove redundant duble negation

--- selectize-plugin-a11y.js
+++ selectize-plugin-a11y.js
@@ -41,7 +41,7 @@ Selectize.define("selectize-plugin-a11y", function(options) {
           } else {
             // option change
             if ($target.hasClass("active")) {
-              if (!!$target.attr("data-value")) {
+              if (!$target.attr("data-value")) {
                 self.$control_input.attr(
                   "aria-activedescendant",
                   $target.attr("id")
